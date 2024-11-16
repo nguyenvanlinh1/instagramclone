889ea2afb6e865daaac46628ec02fa8e5cb6afd0
nguyenvanlinh1 <nvanlinh1406@gmail.com>
2024-11-16 11:47:19 +0700

    continue

--- ins-be/src/main/java/com/nvl/ins_be/config/WebsocketConfig.java
+++ ins-be/src/main/java/com/nvl/ins_be/config/WebsocketConfig.java
@@ -9,12 +9,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // Phương thức này dùng để đăng ký các endpoint STOMP mà client có thể kết nối.
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
                 .setAllowedOriginPatterns("*").withSockJS();
     }
 
+    //Phương thức này dùng để cấu hình message broker, nơi xử lý các tin nhắn được gửi và nhận giữa client và server.
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
--- ins-fe/src/Config/OAuthConfig.js
+++ ins-fe/src/Config/OAuthConfig.js
@@ -1,5 +1,5 @@
 export const OAuthConfig = {
-    clientId: "1040903330486-pjq1nditab2guhaolurvguru7b592m5o.apps.googleusercontent.com",
-    redirectUri: "http://localhost:4545/authenticate",
-    authUri: "https://accounts.google.com/o/oauth2/auth"
+    clientId: "${client-id}",
+    redirectUri: "${redirect-uri}",
+    authUri: "${auth-uri}"
 }
